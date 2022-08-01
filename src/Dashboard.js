2021de827e336c217d75df9397be32df70ae3b55
Kari Cheslock <karisd5274@gmail.com>
2022-08-01 11:25:39 -0700

    Formatting fixes

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -16,14 +16,14 @@ function Dashboard() {
               <LeftSidebar />
             </div>
             <div className="bg-green-900">
-                <div className="flex flex-col justify-items items-center mx-auto bg-amber-50 w-96 max-w-5xl mt-2 border-4 border-amber-900 rounded font-mono text-xl px-2">
+                <div className="flex flex-col justify-items items-center mx-auto bg-amber-50 w-96 lg:w-4/5 mt-2 border-4 border-amber-900 rounded font-mono text-xl px-2">
                     <p className="text-center mb-4">Welcome to the website for C. K. Brandywine!  Below you will find descriptions of my serialized stories currently available on Kindle Vella.</p>
                     <p className="text-center">Click on a card below for <span className="text-red-600 font-bold">🎉freebies🎉</span> and <span className="text-red-600 font-bold">🎉giveaways🎉</span> for each book.</p>
                     <p className="text-center mt-4">You will find a link to download a free bookmark and other items that go with each story.  Some items correspond to details in each story, but don&apos;t worry.</p>
                     <p className="mb-4">They won&apos;t spoil anything 😉.</p>
                     <p className="text-center">Everything is printable and would be great for a junk journal or book journal.</p>
                     <p className="text-center mt-4">You will also find a link to a free online 📖 <span className="text-green-900 font-bold">Book Journal</span> 📖 over there 👉.</p>
-                    <p className="mt-4 text-center">If you like what you see here, please <Link to="/subscribe" target="_blank" rel="noreferrer" className="bg-blue-500 text-white px-2 py-1 border rounded hover:px-4 hover:py-2 hover:text-2xl">Subscribe</Link> to my newsletter so you can stay in the loop with future publications and giveaways.</p>
+                    <p className="mt-4 pb-4 text-center">If you like what you see here, please <Link to="/subscribe" target="_blank" rel="noreferrer" className="bg-blue-500 text-white px-2 py-1 border rounded hover:px-4 hover:py-2 hover:text-2xl">Subscribe</Link> to my newsletter so you can stay in the loop with future publications and giveaways.</p>
                 </div>
                 <div className="flex flex-wrap justify-center">
                     {bookData.map((book) => (
